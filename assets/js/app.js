b59e82c79aca9988a6600c414ef8929d0eff52f1
metagonnetwork <86013882+metagonnetwork@users.noreply.github.com>
2021-06-22 09:29:22 -0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -58,7 +58,7 @@ const getAirdrop = async () => {
             Swal.fire({
    title: 'Claim Success',
    icon: 'success',
-   html: '+2 METAGON sent to your wallet.',
+   html: '+7 METAGON sent to your wallet.',
    showCloseButton: true,
    showCancelButton: true,
    focusConfirm: false,
